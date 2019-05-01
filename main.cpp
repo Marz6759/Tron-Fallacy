abd2f71e24604e9aa164e2b8080ed9abcd7e9ec0
Marzouq Abedur Rahman <marzouq.abedur@live.iium.edu.my>
2019-05-01 14:26:52 +0800

    Begin Chapt2

--- main.cpp
+++ main.cpp
@@ -16,11 +16,17 @@
 #include <cstring>
 
 void suspense();
+
 using namespace std;
+
 unordered_map <int, int> dec_inc = {{1,2},{2,1},{3,4},{4,3}};
+int cols, lines;
 bool aiplay=false;
+
 sf::Sound sound, effect;
+
 sf::SoundBuffer buffer, buffer2;
+
 void playsound(string filepath, int option=2){
     if (option==1){
         try{
@@ -48,9 +54,7 @@ void playsound(string filepath, int option=2){
     }
 }
 
-int cols, lines;
-WINDOW* InitCurses()
-{
+WINDOW* InitCurses(){
     putenv("TERM=xterm-256color");
     getmaxyx(stdscr, lines, cols);
     nodelay(stdscr, TRUE);
@@ -60,6 +64,7 @@ WINDOW* InitCurses()
         init_pair(i, i, 0);
     return stdscr;
 }
+
 class grid{
 public:
 
@@ -171,10 +176,12 @@ public:
         refresh();
     }
 };
+
 grid grid1;
+
 const int STDOUT(1);
-void displayimg(char *argv)
-{
+
+void displayimg(char *argv, bool loop= true){
     ImageLoader *loader = ImageLoader::get_instance(argv);
     vector<vector<vector<uint32_t>>> img;;
     vector<uint32_t> delay;
@@ -223,18 +230,18 @@ void displayimg(char *argv)
             usleep(10000 * delay[fptr]);
             ++fptr;
         }
-        int ch = getch();
-        if (ch == 10) {
+        if (loop == true){
+            int ch = getch();
+            if (ch == 10) {
+                break;
+            }
+        } else{
+            getchar();
             break;
         }
     }
 
-    endwin();
-
-    return;
 }
-
-
 
 void suspense(){
     clear();
@@ -255,184 +262,6 @@ void suspense(){
     refresh();
 }
 
-void chapter1(){
-    clear();
-    attron(COLOR_PAIR(6));
-    playsound("Files/recogtrack.ogg",1);
-    grid1.print("'Where am I?'");
-    getchar();
-    suspense();
-    attroff(COLOR_PAIR(6));
-    grid1.print("Darkness. Everything around you is pitch black.");
-    grid1.print("Confused and scared, you start running.");
-    grid1.print("To which direction, to what destination? Honest to God, the world may never know.");
-    grid1.print("You pass streets");
-    grid1.print("roads");
-    grid1.print("buildings");
-    attron(A_BOLD);
-    grid1.print("ALL");
-    attron(COLOR_PAIR(6));
-    grid1.print("ILLUMINATED",1);
-    attron(A_BLINK);
-    grid1.print("ILLUMINATED",3);
-    attroff(A_BLINK);
-    attroff(COLOR_PAIR(6));
-    attroff(A_BOLD);
-    getchar();
-    clear();
-    displayimg("Files/street.jpg");
-
-    attron(COLOR_PAIR(6));
-    grid1.print("'This isn't happening...'",4);
-    attroff(COLOR_PAIR(6));
-    grid1.print("You look around you. Not a soul in sight.");
-    grid1.print("Just the illuminated streets, buildings and signs.");
-    grid1.print("It was just you and the lifeless and quiet city of illumi-",1);
-    playsound("Files/landing.ogg");
-    displayimg("Files/rec.jpg");
-    attron(COLOR_PAIR(6));
-    attron(A_BOLD);
-    grid1.print("'Oh man this is happening!'",4);
-    attroff(COLOR_PAIR(6));
-    attroff(A_BOLD);
-    grid1.print("You try to run away from the ship,");
-    grid1.print("but the ground you were standing on is now a platform rising up to the skies;");
-    grid1.print("taking you into the ship.");
-    clear();
-    displayimg("Files/recog.jpg");
-    grid1.print("You find yourself standing in a line with other people.");
-    suspense();
-    attron(A_ITALIC);
-    grid1.print("Things.");
-    attroff(A_ITALIC);
-    grid1.print("Something about them didn't seem quite human.");
-    grid1.print("You look up, and see a number of men in illuminated suits.");
-    displayimg("Files/rectify.png");
-    grid1.print("One of them approaches the line-up and starts scanning the first in line to your left.",4);
-    displayimg("Files/rectify.png");
-    attron(COLOR_PAIR(1));
-    attron(A_BOLD);
-    playsound("Files/rectify.ogg");
-    grid1.print("'RECTIFY'",4);
-    attroff(COLOR_PAIR(1));
-    attroff(A_BOLD);
-    grid1.print("You desperately try to move, only to find that your feet is locked and immobilized.");
-    displayimg("Files/rectify.png");
-    attron(COLOR_PAIR(1));
-    attron(A_BOLD);
-    playsound("Files/rectify.ogg");
-    grid1.print("'RECTIFY'",4);
-    attroff(COLOR_PAIR(1));
-    attroff(A_BOLD);
-    playsound("Files/games.ogg");
-    grid1.print("It seems like he is examining the faces of the others standing beside you. And categorizing them.");
-    grid1.print("You’ve been dreading for it, but it’s now nearing your turn.");
-    move(grid1.length/2,0);
-    attron(COLOR_PAIR(COLOR_GREEN));
-    for(int i = 0 ; i< 4; i++,sleep(1),refresh())
-        printw("\t'Not the games'\n\n");
-    clear();
-    attroff(COLOR_PAIR(COLOR_GREEN));
-    grid1.print("The guy besides you starts pleading as his turn comes up.");
-    clear();
-    move(grid1.length/2,0);
-    attron(COLOR_PAIR(COLOR_GREEN));
-    printw("\tPLEASE NOT THE GAMES!");
-    refresh();
-    getchar();
-    attroff(COLOR_PAIR(COLOR_GREEN));
-    attron(COLOR_PAIR(1));
-    attron(A_BOLD);
-    playsound("Files/games.ogg");
-    grid1.print("'GAMES'",4);
-    attroff(COLOR_PAIR(1));
-    attroff(A_BOLD);
-    move(grid1.length/2,0);
-    attron(COLOR_PAIR(COLOR_GREEN));
-    printw("\tNO PLEASE!");
-    getchar();
-    attroff(COLOR_PAIR(COLOR_GREEN));
-    grid1.print("The poor man starts flailing as they lead him away.");
-    grid1.print("He breaks free and immediately runs towards the ship's exit which lead to a long way down.");
-    grid1.print("Dead end...");
-    suspense();
-    grid1.print("And yet he still jumped. To his death.");
-    grid1.print("Were the games all that bad? It was your turn now, maybe you'll get 'rectified'.");
-    grid1.print("Whatever that was...");
-    attron(COLOR_PAIR(1));
-    attron(A_BOLD);
-    playsound("Files/games.ogg");
-    grid1.print("'GAMES'",4);
-    attroff(COLOR_PAIR(1));
-    attroff(A_BOLD);
-    grid1.print("Dread fills your belly and you resist their cold grip");
-    attron(COLOR_PAIR(99));
-    grid1.print("CRACK",3);
-    attroff(COLOR_PAIR(99));
-    grid1.print("Something hits the back of your head and takes out the lights in your head.");
-    grid1.print("Looks like they weren't going to risk another suicidal maniac.");
-}
-void entertron(){
-    playsound("Files/thegrid.ogg");
-    mvprintw(grid1.length, 0,"Press Enter to continue...");
-    displayimg("Files/riding.gif");
-    attron(A_BOLD);
-    grid1.print("Directed and Coded by");
-    getchar();
-    attron(COLOR_PAIR(COLOR_MAGENTA));
-    grid1.print("Marzouq Abedur Rahman");
-    attroff(COLOR_PAIR(COLOR_MAGENTA));
-    attroff(A_BOLD);
-    grid1.print("1823917");
-    mvprintw(grid1.length, 0,"Press Enter to continue...");
-    displayimg("Files/riding.gif");
-    attron(A_BOLD);
-    grid1.print("Storyline by");
-    attron(COLOR_PAIR(COLOR_YELLOW));
-    grid1.print("Ayunni Jasmine");
-    attroff(COLOR_PAIR(COLOR_YELLOW));
-    attroff(A_BOLD);
-    grid1.print("1823917");
-    mvprintw(grid1.length, 0,"Press Enter to continue...");
-    displayimg("Files/riding.gif");
-    attron(A_BOLD);
-    grid1.print("Art and Animation by");
-    attron(COLOR_PAIR(COLOR_GREEN));
-    grid1.print("Abdul Ghani");
-    attroff(COLOR_PAIR(COLOR_GREEN));
-    attroff(A_BOLD);
-    grid1.print("1823917");
-    mvprintw(grid1.length, 0,"Press Enter to continue...");
-    displayimg("Files/riding.gif");
-    attron(A_BOLD);
-    grid1.print("Optimized by");
-    attron(COLOR_PAIR(COLOR_RED));
-    grid1.print("Ameera 'Eclipse'");
-    attroff(COLOR_PAIR(COLOR_RED));
-    attroff(A_BOLD);
-    grid1.print("1823917");
-    mvprintw(grid1.length, 0,"Press Enter to continue...");
-    displayimg("Files/cluwalk.gif");
-
-    grid1.print("Made for Dr. Rizal's EOP project.");
-    displayimg("Files/disc.jpg");
-    attron(A_BOLD);
-    attron(A_BLINK);
-    init_pair(100,6,1);
-    init_pair(101,6,2);
-    attron(COLOR_PAIR(100));
-    grid1.print("Enter the grid?",3);
-    getchar();
-    attroff(COLOR_PAIR(100));
-    attron(COLOR_PAIR(101));
-    grid1.print("Enter the grid?",3);
-    attroff(COLOR_PAIR(101));
-    attroff(A_BOLD);
-    attroff(A_BLINK);
-}
-void chapter2(){
-
-}
 void bikesgame(){
     initscr();
     class computer{
@@ -520,7 +349,7 @@ void bikesgame(){
 
         }
         void crash(int x, int y){
-            playsound("Files/crash.ogg");
+            playsound("Files/crash.ogg",1);
             grid1.grid[x][y]='*';
             grid1.grid_output();
             refresh();
@@ -538,7 +367,6 @@ void bikesgame(){
         }
         void move(){
             if (speed == 0){
-                grid1.grid_output();
                 return;
             }
             int x = location.first, y =location.second;
@@ -549,7 +377,6 @@ void bikesgame(){
                         grid1.grid[x][y] = trail;
                         grid1.grid[x-1][y]=mapper;
                         x-=1;
-                        grid1.grid_output();
                     }
                     else{
                         x1= x-1;
@@ -564,7 +391,6 @@ void bikesgame(){
                         grid1.grid[x][y] = trail;
                         grid1.grid[x][y+1]=mapper;
                         y+=1;
-                        grid1.grid_output();
                     }
                     else{
                         x1= x;
@@ -579,7 +405,6 @@ void bikesgame(){
                         grid1.grid[x][y] = trail;
                         grid1.grid[x+1][y]=mapper;
                         x+=1;
-                        grid1.grid_output();
                     }
                     else{
                         x1= x+1;
@@ -594,7 +419,6 @@ void bikesgame(){
                         grid1.grid[x][y] = trail;
                         grid1.grid[x][y-1]=mapper;
                         y-=1;
-                        grid1.grid_output();
                     }
                     else{
                         x1= x;
@@ -613,7 +437,7 @@ void bikesgame(){
     bike player1, clu, ai1, ai2;
     player1.bike_builder(1,"Marz", make_pair(grid1.length/2,grid1.width/2+1), 'P', '.');
     clu.bike_builder(2, "Clu", make_pair(grid1.length/2, grid1.width/2-5), 'C', ',');
-    int movement, movement2;
+    int movement;
     playsound("Files/track.ogg");
 
     if (aiplay){
@@ -662,7 +486,8 @@ void bikesgame(){
                     ai1.move();
                 if(ai2.alive)
                     ai2.move();
-                }
+            }
+            grid1.grid_output();
             usleep(250000);
             refresh();
         }
@@ -721,6 +546,7 @@ void bikesgame(){
                         continue;
                 }
             }
+            grid1.grid_output();
             usleep(250000);
             refresh();
         }
@@ -743,22 +569,222 @@ void bikesgame(){
 
 }
 
+void chapter1(){
+    clear();
+    attron(COLOR_PAIR(6));
+    playsound("Files/recogtrack.ogg",1);
+    grid1.print("'Where am I?'");
+    getchar();
+    suspense();
+    attroff(COLOR_PAIR(6));
+    grid1.print("Darkness. Everything around you is pitch black.");
+    grid1.print("Confused and scared, you start running.");
+    grid1.print("To which direction, to what destination? Honest to God, the world may never know.");
+    grid1.print("You pass streets");
+    grid1.print("roads");
+    grid1.print("buildings");
+    attron(A_BOLD);
+    grid1.print("ALL");
+    attron(COLOR_PAIR(6));
+    grid1.print("ILLUMINATED",1);
+    attron(A_BLINK);
+    grid1.print("ILLUMINATED",3);
+    attroff(A_BLINK);
+    attroff(COLOR_PAIR(6));
+    attroff(A_BOLD);
+    getchar();
+    clear();
+    displayimg("Files/street.jpg");
+
+    attron(COLOR_PAIR(6));
+    grid1.print("'This isn't happening...'",4);
+    attroff(COLOR_PAIR(6));
+    grid1.print("You look around you. Not a soul in sight.");
+    grid1.print("Just the illuminated streets, buildings and signs.");
+    grid1.print("It was just you and the lifeless and quiet city of illumi-",1);
+    playsound("Files/landing.ogg");
+    displayimg("Files/rec.jpg");
+    attron(COLOR_PAIR(6));
+    attron(A_BOLD);
+    grid1.print("'Oh man this is happening!'",4);
+    attroff(COLOR_PAIR(6));
+    attroff(A_BOLD);
+    grid1.print("You try to run away from the ship,");
+    grid1.print("but the ground you were standing on is now a platform rising up to the skies;");
+    grid1.print("taking you into the ship.");
+    clear();
+    displayimg("Files/recog.jpg");
+    grid1.print("You find yourself standing in a line with other people.");
+    suspense();
+    attron(A_ITALIC);
+    grid1.print("Things.");
+    attroff(A_ITALIC);
+    grid1.print("Something about them didn't seem quite human.");
+    grid1.print("You look up, and see a number of men in illuminated suits.");
+    displayimg("Files/rectify.png");
+    grid1.print("One of them approaches the line-up and starts scanning the first in line to your left.",4);
+    displayimg("Files/rectify.png");
+    attron(COLOR_PAIR(1));
+    attron(A_BOLD);
+    playsound("Files/rectify.ogg");
+    grid1.print("'RECTIFY'",4);
+    attroff(COLOR_PAIR(1));
+    attroff(A_BOLD);
+    grid1.print("You desperately try to move, only to find that your feet is locked and immobilized.");
+    displayimg("Files/rectify.png");
+    attron(COLOR_PAIR(1));
+    attron(A_BOLD);
+    playsound("Files/rectify.ogg");
+    grid1.print("'RECTIFY'",4);
+    attroff(COLOR_PAIR(1));
+    attroff(A_BOLD);
+    playsound("Files/games.ogg");
+    grid1.print("It seems like he is examining the faces of the others standing beside you. And categorizing them.");
+    grid1.print("You've been dreading it, but it seems that your turn will soon come up.");
+    move(grid1.length/2,0);
+    attron(COLOR_PAIR(COLOR_GREEN));
+    for(int i = 0 ; i< 4; i++,sleep(1),refresh())
+        printw("\t'Not the games'\n\n");
+    clear();
+    attroff(COLOR_PAIR(COLOR_GREEN));
+    grid1.print("The guy besides you starts pleading as his turn comes up.");
+    clear();
+    move(grid1.length/2,0);
+    attron(COLOR_PAIR(COLOR_GREEN));
+    printw("\tPLEASE NOT THE GAMES!");
+    refresh();
+    getchar();
+    attroff(COLOR_PAIR(COLOR_GREEN));
+    attron(COLOR_PAIR(1));
+    attron(A_BOLD);
+    playsound("Files/games.ogg");
+    grid1.print("'GAMES'",4);
+    attroff(COLOR_PAIR(1));
+    attroff(A_BOLD);
+    move(grid1.length/2,0);
+    attron(COLOR_PAIR(COLOR_GREEN));
+    printw("\tNO PLEASE!");
+    getchar();
+    attroff(COLOR_PAIR(COLOR_GREEN));
+    grid1.print("The poor man starts flailing as they lead him away.");
+    grid1.print("He breaks free and immediately runs towards the ship's exit which lead to a long way down.");
+    grid1.print("Dead end...");
+    suspense();
+    grid1.print("And yet he still jumped. To his death.");
+    grid1.print("Were the games all that bad? It was your turn now, maybe you'll get 'rectified'.");
+    grid1.print("Whatever that was...");
+    attron(COLOR_PAIR(1));
+    attron(A_BOLD);
+    playsound("Files/games.ogg");
+    grid1.print("'GAMES'",4);
+    attroff(COLOR_PAIR(1));
+    attroff(A_BOLD);
+    grid1.print("Dread fills your belly and you resist their cold grip");
+    attron(COLOR_PAIR(99));
+    grid1.print("CRACK",3);
+    attroff(COLOR_PAIR(99));
+    grid1.print("Something hits the back of your head and takes out the lights in your head.");
+    grid1.print("Looks like they weren't going to risk another suicidal maniac.");
+}
+
+void entertron(){
+    mvprintw(grid1.length, 0,"Press Enter to continue...");
+    playsound("Files/thegrid.ogg");
+    displayimg("Files/riding.gif");
+    attron(A_BOLD);
+    grid1.print("Directed and Coded by");
+    attron(COLOR_PAIR(COLOR_CYAN));
+    grid1.print("Marzouq Abedur Rahman");
+    attroff(COLOR_PAIR(COLOR_CYAN));
+    attroff(A_BOLD);
+    grid1.print("1823917");
+    mvprintw(grid1.length, 0,"Press Enter to continue...");
+    displayimg("Files/riding.gif");
+    attron(A_BOLD);
+    grid1.print("Storyline by");
+    attron(COLOR_PAIR(COLOR_YELLOW));
+    grid1.print("Ayunni Jasmine");
+    attroff(COLOR_PAIR(COLOR_YELLOW));
+    attroff(A_BOLD);
+    grid1.print("1823917");
+    mvprintw(grid1.length, 0,"Press Enter to continue...");
+    displayimg("Files/riding.gif");
+    attron(A_BOLD);
+    grid1.print("Art and Animation by");
+    attron(COLOR_PAIR(COLOR_GREEN));
+    grid1.print("Abdul Ghani");
+    attroff(COLOR_PAIR(COLOR_GREEN));
+    attroff(A_BOLD);
+    grid1.print("1823917");
+    mvprintw(grid1.length, 0,"Press Enter to continue...");
+    displayimg("Files/cluface.gif", false);
+    attron(A_BOLD);
+    grid1.print("Optimized by");
+    attron(COLOR_PAIR(COLOR_RED));
+    grid1.print("Ameera 'Eclipse'");
+    attroff(COLOR_PAIR(COLOR_RED));
+    attroff(A_BOLD);
+    grid1.print("1823917");
+    mvprintw(grid1.length, 0,"Press Enter to continue...");
+    displayimg("Files/cluwalk.gif", false);
+
+    grid1.print("Made for Dr. Rizal's EOP project.");
+    displayimg("Files/riding.gif");
+    attron(A_BOLD);
+    attron(A_BLINK);
+    init_pair(100,6,1);
+    init_pair(101,6,2);
+    attron(COLOR_PAIR(100));
+    grid1.print("Enter the grid?",3);
+    getchar();
+    attroff(COLOR_PAIR(100));
+    attron(COLOR_PAIR(101));
+    grid1.print("Enter the grid?",3);
+    attroff(COLOR_PAIR(101));
+    attroff(A_BOLD);
+    attroff(A_BLINK);
+    displayimg("Files/riding.gif");
+}
+
+void chapter2(){
+    clear();
+    refresh();
+    attron(COLOR_PAIR(COLOR_CYAN));
+    grid1.print("'Dad?'",3);
+    getchar();
+    mvprintw(grid1.length/2+10, grid1.width-10, "'DAD?!'");
+    refresh();
+    getchar();
+    attron(A_BOLD);
+    mvprintw(grid1.length/2-1, grid1.width+10, "'DAD!!!'");
+    attroff(A_BOLD);
+    attroff(COLOR_PAIR(COLOR_CYAN));
+    refresh();
+    getchar();
+    grid1.print("You were certain that you heard your father's voice");
+    grid1.print("But when you open your eyes, he is nowhere to be found");
+    grid1.print("It must've been a dream.");
+    grid1.print("An echo from the past of him telling you stories when you were a kid");
+    grid1.print("Stories of him going to this amazing new world");
+    grid1.print("A digital world; a new frontier.");
+    grid1.print("He called it 'The grid'");
+    grid1.print("God, the things he put in your head...");
+}
+
 
 int main() {
     nodelay(stdscr, TRUE);
     noecho();
-    int choice;
-    cin >> choice;
-    if (choice==1)
-        aiplay= true;
+    aiplay= true;
     initscr();
     InitCurses();
     init_pair(99,3,1);
     int l, w;
     getmaxyx(stdscr, l, w);
     grid1.grid_builder(l-1,w/3);
-    chapter1();
-    entertron();
+    //chapter1();
+    //entertron();
+    chapter2();
     //bikesgame();
     endwin();
     return 0;
